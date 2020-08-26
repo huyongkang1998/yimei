7a36e3992518f816a3be46deba4cf1bb0beca165
huyongkang <2250676177@qq.com>
2020-08-26 22:32:47 +0800

    第一次python课程

--- demo.py
+++ demo.py
@@ -1,1 +1,14 @@
-print("1fghf11")
+# print("1fghf11",end="")
+# print("123123131",123123421,"124124124")
+# print("哈哈"+"喜喜")
+# print(1>3)
+# name = "张三"
+# print(name)
+# res = 1+2*3+4/2
+# print(res)
+# a = int(input("请输入a："))
+# b = int(input("请输入b："))
+# print(a+b)
+a = len(input("请输入："))
+b = a%2
+print(b)
